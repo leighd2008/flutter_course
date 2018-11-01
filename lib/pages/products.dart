b76dc007127b8b3f20bb022647dda8f346fc8134
leighd2008 <leighd2008@gmail.com>
2018-11-01 12:47:45 -0400

    176 Using Pull to Refresh

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -11,9 +11,9 @@ class ProductsPage extends StatefulWidget {
   ProductsPage(this.model);
 
   @override
-    State<StatefulWidget> createState() {
-      return _ProductsPageState();
-    }
+  State<StatefulWidget> createState() {
+    return _ProductsPageState();
+  }
 }
 
 class _ProductsPageState extends State<ProductsPage> {
@@ -44,15 +44,20 @@ class _ProductsPageState extends State<ProductsPage> {
   }
 
   Widget _buildProductsList() {
-    return ScopedModelDescendant(builder: (BuildContext context, Widget child, MainModel model) {
-      Widget content = Center(child: Text('No Products Found'));
-      if (model.displayedProducts.length > 0 && !model.isLoading) {
-        content = Products();
-      } else if (model.isLoading) {
-        content = Center(child: CircularProgressIndicator());
-      }
-      return content;
-    },);
+    return ScopedModelDescendant(
+      builder: (BuildContext context, Widget child, MainModel model) {
+        Widget content = Center(child: Text('No Products Found'));
+        if (model.displayedProducts.length > 0 && !model.isLoading) {
+          content = Products();
+        } else if (model.isLoading) {
+          content = Center(child: CircularProgressIndicator());
+        }
+        return RefreshIndicator(
+          onRefresh: model.fetchProducts,
+          child: content,
+        );
+      },
+    );
   }
 
   @override
