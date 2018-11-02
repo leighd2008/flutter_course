d9d0238f4e8442eb54025cfa5155a66858f981fe
leighd2008 <leighd2008@gmail.com>
2018-11-02 13:35:55 -0400

    195 Storing the Token on the Device

--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 
 import 'package:scoped_model/scoped_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/product.dart';
 import '../models/user.dart';
@@ -253,6 +254,8 @@ class UserModel extends ConnectedProductsModel {
           id: responseData['localId'],
           email: email,
           token: responseData['idToken']);
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString('token', responseData['idToken']);
     } else if (responseData['error']['message'] == 'EMAIL_EXISTS') {
       message = 'This email already exists.';
     } else if (responseData['error']['message'] == 'EMAIL_NOT_FOUND') {
@@ -263,6 +266,14 @@ class UserModel extends ConnectedProductsModel {
     _isLoading = false;
     notifyListeners();
     return {'success': !hasError, 'message': message};
+  }
+}
+
+void autoAuthenticate() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final String token = prefs.getString('token');
+  if (token != null) {
+    
   }
 }
 
