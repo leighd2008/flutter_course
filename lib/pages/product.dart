d3f20fbffe26672ff4623506cc9f82886d637740
leighd2008 <leighd2008@gmail.com>
2018-11-13 07:10:22 -0500

    #225 Showing a Fullscreen Map

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -2,24 +2,52 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-// import 'package:scoped_model/scoped_model.dart';
+import 'package:map_view/map_view.dart';
 
 import '../widgets/ui_elements/title_default.dart';
 import '../models/product.dart';
-// import '../scoped-models/main.dart';
 
 class ProductPage extends StatelessWidget {
   final Product product;
 
   ProductPage(this.product);
 
+  void _showMap() {
+    final List<Marker> markers = <Marker>[
+      Marker('position', 'Position', product.location.latitude,
+          product.location.longitude)
+    ];
+    final cameraPosition = CameraPosition(
+        Location(product.location.latitude, product.location.longitude), 14.0);
+    final mapView = MapView();
+    mapView.show(
+        MapOptions(
+            initialCameraPosition: cameraPosition,
+            mapViewType: MapViewType.normal,
+            title: 'Product Location'),
+        toolbarActions: [
+          ToolbarAction('Close', 1),
+        ]);
+    mapView.onToolbarAction.listen((int id) {
+      if (id == 1) {
+        mapView.dismiss();
+      }
+    });
+    mapView.onMapReady.listen((_) {
+      mapView.setMarkers(markers);
+    });
+  }
+
   Widget _buildAddressPriceRow(String address, double price) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text(
-          address,
-          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        GestureDetector(
+          onTap: _showMap,
+          child: Text(
+            address,
+            style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+          ),
         ),
         Container(
           margin: EdgeInsets.symmetric(horizontal: 5.0),
