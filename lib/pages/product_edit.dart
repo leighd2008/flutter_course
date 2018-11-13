f81a7d0f0730370b02b4b46d8fec50d68306f69d
leighd2008 <leighd2008@gmail.com>
2018-11-13 09:03:20 -0500

    #229 Adding an Image Picker Button

--- lib/pages/product_edit.dart
+++ lib/pages/product_edit.dart
@@ -7,6 +7,7 @@ import '../widgets/form_inputs/location.dart';
 import '../models/product.dart';
 import '../scoped-models/main.dart';
 import '../models/location_data.dart';
+import '../widgets/form_inputs/image.dart';
 
 class ProductEditPage extends StatefulWidget {
   @override
@@ -150,6 +151,10 @@ class _ProductEditPageState extends State<ProductEditPage> {
                 height: 10.0,
               ),
               LocationInput(_setLocation, product),
+              SizedBox(
+                height: 10.0,
+              ),
+              ImageInput(),
               SizedBox(
                 height: 10.0,
               ),
