e2fe4b3e5431271469df1099cf8c82ecad3580c6
leighd2008 <leighd2008@gmail.com>
2018-11-16 07:28:52 -0500

    #237 Reading Incoming Files Fields

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const functions = require('firebase-functions');
-const cors = require('cors')({origin: true});
+const cors = require('cors')({ origin: true });
+const Busboy = require('busboy');
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,16 +15,32 @@ const cors = require('cors')({origin: true});
 exports.storeImage = functions.https.onRequest((req, res) => {
   return cors(req, res, () => {
     if (req.method !== 'POST') {
-      return res.status(500).json({message: 'Not allowed.'});
+      return res.status(500).json({ message: 'Not allowed.' });
     }
 
     if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
-      return res.status(401).json({error: 'Unauthorized.'});
+      return res.status(401).json({ error: 'Unauthorized.' });
     }
 
     let idToken;
     idToken = req.headers.authorization.split('Bearer ')[1];
 
-    
+    const busboy = new Busboy({ headers: req.headers });
+    let uploadData;
+    let oldImagePath;
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+      const filePath = path.join(os.tmpdir(), filename);
+      uploadData = { filePath: filePath, type: mimetype, name: filename };
+      file.pipe(fs.createWriteStream(filePath));
+    });
+
+    busboy.on('field', (fieldname, value) => {
+      oldImagePath = decodeURIComponent(value);
+    })
+
+    busboy.on('finish', () => {
+      
+    });
   });
 });
