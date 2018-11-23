153d7f6ca174e5e30dfc9317aac0ec21315229e4
leighd2008 <leighd2008@gmail.com>
2018-11-23 10:53:19 -0500

    #271 Adding Space on the Product Card

--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -80,6 +80,9 @@ class ProductCard extends StatelessWidget {
             ),
           ),
           _buildTitlePriceRow(),
+          SizedBox(
+            height: 10.0,
+          ),
           AddressTag(product.location.address),
           _buildActionButtons(context)
         ],
